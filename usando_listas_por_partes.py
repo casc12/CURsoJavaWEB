8ecb7954e49621a597643d82018d51980094e198
Carlos Augusto de Sousa <41213544+casc12@users.noreply.github.com>
2025-03-07 15:52:12 -0300

    Update usando_listas_por_partes.py

--- usando_listas_por_partes.py
+++ usando_listas_por_partes.py
@@ -1,61 +1,7 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyOEcExpdALf/eVvvYivGcHN",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/casc12/CURsoJavaWEB/blob/master/usando_listas_por_partes.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "execution_count": 3,
-      "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "id": "_JD2gfW5aKJr",
-        "outputId": "dff98a69-3dd6-44a6-eb4a-e8f77add42aa"
-      },
-      "outputs": [
-        {
-          "output_type": "stream",
-          "name": "stdout",
-          "text": [
-            "['banana', 'maçã', 'abacaxi']\n",
-            "maçã\n"
-          ]
-        }
-      ],
-      "source": [
-        "\"\"\"texto = \"Curso de Lógica\"\n",
-        "\n",
-        "for x in texto:\n",
-        "  print(x)\"\"\"\n",
-        "compras = [\"banana\",\"maçã\", \"abacaxi\"]\n",
-        "print(compras)\n",
-        "print(compras[1])\n",
-        "#d\n"
-      ]
-    }
-  ]
-}
+"""texto = "Curso de Lógica"
+
+for x in texto:
+  print(x)"""
+compras = ["banana","maçã", "abacaxi"]
+print(compras)
+print(compras[1])
